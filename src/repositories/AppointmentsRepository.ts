b27ae7e89b8451a093d7936a2a19ce66857e3406
AntDavidLima <DavidLima@alu.ufc.br>
2020-06-28 14:16:11 -0300

    Created Appointments repository; Code refactoring

---
+++ src/repositories/AppointmentsRepository.ts
@@ -0,0 +1,27 @@
+import { isEqual } from 'date-fns';
+
+import Appointment from '../models/Appointment';
+
+class AppointmentsRepository {
+  private appointment: Appointment[];
+
+  constructor() {
+    this.appointment = [];
+  }
+
+  public findByDate(date: Date): Appointment | null {
+    const duplicatedAppointment = this.appointment.find(appointment =>
+      isEqual(date, appointment.date),
+    );
+
+    return duplicatedAppointment || null;
+  }
+
+  public create(provider: string, date: Date): Appointment {
+    const appointment = new Appointment(provider, date);
+    this.appointment.push(appointment);
+    return appointment;
+  }
+}
+
+export default AppointmentsRepository;
--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,28 +1,25 @@
 import { Router } from 'express';
-import { startOfHour, parseISO, isEqual } from 'date-fns';
+import { startOfHour, parseISO } from 'date-fns';
 
-import Appointment from '../models/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 const appointmentsRouter = Router();
-
-const appointments: Appointment[] = [];
+const appointmentsRepository = new AppointmentsRepository();
 
 appointmentsRouter.post('/', (req, res) => {
   const { provider, date } = req.body;
 
   const parsedDate = startOfHour(parseISO(date));
 
-  const duplicatedDateAppointment = appointments.find(appointment =>
-    isEqual(parsedDate, appointment.date),
+  const findAppointmentInSameDate = appointmentsRepository.findByDate(
+    parsedDate,
   );
 
-  if (duplicatedDateAppointment) {
+  if (findAppointmentInSameDate) {
     return res.status(400).json({ error: 'This date is already booked' });
   }
 
-  const appointment = new Appointment(provider, parsedDate);
-
-  appointments.push(appointment);
+  const appointment = appointmentsRepository.create(provider, parsedDate);
 
   return res.json(appointment);
 });
