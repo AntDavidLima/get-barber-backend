965b68756de7b4f5e831c6468844db4ba0e1be3d
AntDavidLima <DavidLima@alu.ufc.br>
2020-06-29 17:15:25 -0300

    Configured repositories, routes and serives to use database

--- src/repositories/AppointmentsRepository.ts
+++ src/repositories/AppointmentsRepository.ts
@@ -1,35 +1,15 @@
-import { isEqual } from 'date-fns';
+import { EntityRepository, Repository } from 'typeorm';
 
 import Appointment from '../models/Appointment';
 
-interface CreateAppointmentDTO {
-  provider: string;
-  date: Date;
-}
-
-class AppointmentsRepository {
-  private appointment: Appointment[];
-
-  constructor() {
-    this.appointment = [];
-  }
-
-  public all(): Appointment[] {
-    return this.appointment;
-  }
-
-  public findByDate(date: Date): Appointment | null {
-    const duplicatedAppointment = this.appointment.find(appointment =>
-      isEqual(date, appointment.date),
-    );
+@EntityRepository(Appointment)
+class AppointmentsRepository extends Repository<Appointment> {
+  public async findByDate(date: Date): Promise<Appointment | null> {
+    const duplicatedAppointment = await this.findOne({
+      where: { date },
+    });
 
     return duplicatedAppointment || null;
-  }
-
-  public create({ provider, date }: CreateAppointmentDTO): Appointment {
-    const appointment = new Appointment({ provider, date });
-    this.appointment.push(appointment);
-    return appointment;
   }
 }
 
