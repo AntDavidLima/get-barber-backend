c33153e40318f859dde4ed07fa63e96fb4240a7b
AntDavidLima <DavidLima@alu.ufc.br>
2020-06-30 13:22:04 -0300

    Created Users Migration and Model

--- src/models/User.ts
+++ src/models/User.ts
@@ -3,24 +3,28 @@ import {
   Column,
   PrimaryGeneratedColumn,
   CreateDateColumn,
+  UpdateDateColumn,
 } from 'typeorm';
 
-@Entity('appointments')
-class Appointment {
+@Entity('users')
+class Users {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
   @Column()
-  provider: string;
+  name: string;
 
-  @Column('time with time zone')
-  date: Date;
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
 
   @CreateDateColumn()
   created_at: Date;
 
-  @CreateDateColumn()
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
-export default Appointment;
+export default Users;
