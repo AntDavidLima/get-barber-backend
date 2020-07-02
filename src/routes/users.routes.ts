bfa96905739d8f22f5878eb6197692da02759185
AntDavidLima <DavidLima@alu.ufc.br>
2020-07-02 10:01:12 -0300

    Files upload

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import CreateUserService from '../services/CreateUserService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import uploadConfig from '../config/upload';
 
 const usersRouter = Router();
+
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (req, res) => {
   try {
@@ -19,5 +24,14 @@ usersRouter.post('/', async (req, res) => {
     return res.status(400).json({ error: e.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  (req, res) => {
+    return res.json({ ok: 'true' });
+  },
+);
 
 export default usersRouter;
